c13dba55ebfc508d5045c310d36c39b481107705
ciriast <ciriastorres@gmail.com>
2024-11-28 17:46:21 -0600

    Allow only one character

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 int main() {
     printf("Start game!\n");
     
-    char characterFromUser[50];
-
+    char characterFromUser[2];
+    
+    printf("Note: You can enter only one letter, if you enter more than one, only the first character will be take it.\n");
     printf("Enter a letter:");
 
     fgets(characterFromUser, sizeof(characterFromUser), stdin);
