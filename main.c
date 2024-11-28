bb2c17c9be43d768e278ce48e69327e063bfa2bd
ciriast <ciriastorres@gmail.com>
2024-11-27 20:59:22 -0600

    feat: Add the process to read the user input

--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 
 int main() {
     printf("Start game!\n");
+    
+    char characterFromUser[50];
+
+    printf("Enter a character:");
+
+    fgets(characterFromUser, sizeof(characterFromUser), stdin);
+    printf("You entered: %s", characterFromUser);
+
     return 0;
 }
