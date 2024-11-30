b1401ae19301f64c12e4adca658a409fa3eb9f30
ciriast <ciriastorres@gmail.com>
2024-11-29 20:05:33 -0600

    Add the word to guess

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main() {
     printf("Start game!\n");
-    
+    char wordToGuess[5] = "hello";    
     char characterFromUser[2];
     
     printf("Note: You can enter only one letter, if you enter more than one, only the first character will be take it.\n");
