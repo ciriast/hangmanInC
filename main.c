bafcd0294f31e17e89bf6c2c66d62acec1448deb
ciriast <ciriastorres@gmail.com>
2024-11-30 11:08:59 -0600

    Testing new solution

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <ctype.h>
 #include <string.h>
 
+void clear_input_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+// Ok, this buffer thing is a little confusing, seems the stdin saves thing that I don't want to the rest of the while loop, so I need to manually clean it.
+// I don't know why this little details aren't mentioned in the docs.
+// I needed help to understand the buffer thing, it's confusing
 int main() {
     printf("Start game!\n");
     char wordToGuess[5] = "hello";    
@@ -10,21 +18,22 @@ int main() {
     printf("Note: You can enter only one letter, if you enter more than one, only the first character will be take it.\n");
 
     while (remaining_lives > 0) {
+        char my_input[10];
         char characterFromUser;
 
-        printf("Debugging character %c\n", characterFromUser);
         printf("You have %d remaining lives\n", remaining_lives);
         printf("Enter a letter:");
 
-        scanf(" %c", &characterFromUser);
-    
-        characterFromUser = tolower(characterFromUser);
+        fgets(my_input, sizeof(my_input), stdin);
+
+        characterFromUser = tolower(my_input[0]);
         printf("You entered: %c\n", characterFromUser);
 
         if (characterFromUser < 'a' || characterFromUser > 'z') {
             printf("The character is not an alphabet.\n");
         }
-
+        
+        clear_input_buffer();
         --remaining_lives;
     }
 
