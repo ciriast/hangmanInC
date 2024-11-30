96fc1dd622939461583d84e69c3325a27393fea9
ciriast <ciriastorres@gmail.com>
2024-11-29 20:19:34 -0600

    Add the remaining_lives variable

--- main.c
+++ main.c
@@ -5,8 +5,10 @@ int main() {
     printf("Start game!\n");
     char wordToGuess[5] = "hello";    
     char characterFromUser[2];
-    
+    int remaining_lives = 3;
+
     printf("Note: You can enter only one letter, if you enter more than one, only the first character will be take it.\n");
+    printf("You have %d remaining lives\n", remaining_lives);
     printf("Enter a letter:");
 
     fgets(characterFromUser, sizeof(characterFromUser), stdin);
@@ -15,7 +17,7 @@ int main() {
     printf("You entered: %c\n", characterToUse);
 
     if (characterToUse < 'a' || characterToUse > 'z') {
-        printf("The character is not an alphabet.");
+        printf("The character is not an alphabet.\n");
         return 0;
     }
 
